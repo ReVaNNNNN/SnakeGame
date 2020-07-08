34e21681238378f0fd1d9ef7c59c01fb9d9afa06
ReVaN <revannn5480@gmail.com>
2020-07-08 12:24:43 +0200

    Snake: create board

---
+++ snake/src/Game.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import Cells from "./Cells";
+import { START, BODY, FOOD, KEYS, COLS, ROWS } from "./const";
+import "./style.css";
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      board: [],
+      snake: [],
+      direction: null,
+      gameOver: false,
+    };
+
+    this.start = this.start.bind(this);
+    this.frame = this.frame.bind(this);
+  }
+
+  componentDidMount() {
+    this.start();
+  }
+
+  start() {
+    const board = [];
+    const snake = [START];
+
+    board[START] = BODY;
+
+    this.setState(
+      {
+        board,
+        snake,
+        direction: KEYS.right,
+      },
+      () => {
+        this.frame();
+      }
+    );
+  }
+
+  frame() {
+    const { snake, board, direction } = this.state;
+
+    const head = this.getNextIndex(snake[0], direction);
+    snake.unshift(head);
+
+    board[head] = BODY;
+    board[snake.pop()] = null;
+
+    this.setState(
+      {
+        board,
+        snake,
+      },
+      () => {
+        setTimeout(this.frame, 200);
+      }
+    );
+  }
+
+  getNextIndex(head, direction) {
+    let x = head % COLS;
+    let y = Math.floor(head, COLS);
+
+    switch (direction) {
+      case KEYS.up:
+        y = y <= 0 ? ROWS - 1 : y - 1;
+        break;
+      case KEYS.down:
+        y = y >= ROWS ? 0 : y + 1;
+        break;
+      case KEYS.left:
+        x = x <= 0 ? COLS - 1 : x - 1;
+        break;
+      case KEYS.right:
+        x = x >= COLS - 1 ? 0 : x + 1;
+        break;
+      default:
+        return;
+    }
+
+    return COLS * y + x;
+  }
+
+  render() {
+    const { board } = this.state;
+
+    return <Cells board={board} />;
+  }
+}
+
+export default Game;
